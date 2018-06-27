b0c23f6ea30ac5d25a48cde0359bd25d25159958
YuriiZadorozhnyi <yurij_zadoroznij@ukr.net>
2018-06-27 18:48:50 +0300

    confirmation modal refactoring

--- src/app/common/share/modals/shared-modal-provider.service.ts
+++ src/app/common/share/modals/shared-modal-provider.service.ts
@@ -7,11 +7,8 @@ import { ConfirmationModalComponent } from './confirmation-modal/confirmation-mo
 export class SharedModalProvider {
   constructor(private dialog: MatDialog) {}
 
-  openConfirmationModal(title, question, cancelBtn, okBtn): MatDialogRef<ConfirmationModalComponent> {
-    return this.dialog.open(ConfirmationModalComponent, {
-      data: { title, question, cancelBtn, okBtn },
-      maxHeight: '90%'
-    });
+  openConfirmationModal(data): MatDialogRef<ConfirmationModalComponent> {
+    return this.dialog.open(ConfirmationModalComponent, { data, maxHeight: '90%' });
   }
 
 }
