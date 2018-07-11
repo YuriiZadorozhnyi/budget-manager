6a727c68534a9d600dbd6aa56f6f486a8b8d5cd5
YuriiZadorozhnyi <yurij_zadoroznij@ukr.net>
2018-07-11 23:08:50 +0300

    update to angular v6 and angular material v6

--- src/app/common/core/interceptors/global.interceptor.ts
+++ src/app/common/core/interceptors/global.interceptor.ts
@@ -3,7 +3,7 @@ import {
   HttpEvent, HttpInterceptor, HttpHandler, HttpRequest
 } from '@angular/common/http';
 
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs/observable';
 
 import { environment } from '@environments/environment';
 
